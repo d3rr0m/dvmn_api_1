285864785bfe7faf3c9d96aa22180f160846469a
Roman Deriga <deriga.roman@gmail.com>
2023-08-08 12:36:31 +0300

    Add loop

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ import requests
 URL = 'https://wttr.in/'
 payload = {
     'MnTQ': '',
-    '%26': '',
     'lang': 'ru',
 }
 
 
-def get_weather(city_code: str, payload):
-    response = requests.get(URL, params=payload)
+def get_weather(city_code: str):
+    response = requests.get(URL+city_code, params=payload)
     response.raise_for_status()
     print(response.url)
-    return response
+    return response.text
 
 
 def main():
-    print(get_weather('london', 'ru', 'MnTq').text)
-    print(get_weather('svo', 'ru', 'MnTq').text)
-    print(get_weather('череповец', 'ru', 'MnTq').text)
+    places = ['london', 'svo', 'череповец']
+    for place in places:
+        print(get_weather(place))
 
 
 if __name__ == '__main__':
