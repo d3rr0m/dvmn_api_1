d8c8f0749ce1ec04f0bef8a4d54397ac19f70885
Roman Deriga <deriga.roman@gmail.com>
2023-08-09 09:00:47 +0700

    Replace concat with f-string

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import requests
 
 
 URL = 'https://wttr.in/'
-payload = {
-    'MnTQ': '',
-    'lang': 'ru',
-}
 
 
-def get_weather(city_code: str):
-    response = requests.get(URL+city_code, params=payload)
+def get_weather(city_code: str, payload: dict):
+    response = requests.get(f'{URL}{city_code}', params=payload)
     response.raise_for_status()
     return response.text
 
 
 def main():
+    payload = {
+    'MnTQ': '',
+    'lang': 'ru',
+    }
     places = ['london', 'svo', 'череповец']
     for place in places:
-        print(get_weather(place))
+        print(get_weather(place, payload))
 
 
 if __name__ == '__main__':
